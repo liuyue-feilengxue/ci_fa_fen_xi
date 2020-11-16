e3698f2fce3adb3e2c1abf28da2087097d086695
liuyuefeilengxue <1365119439@qq.com>
2020-11-16 19:53:57 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+//单词的结构体
+typedef struct
+{
+    int typenum;//类型编码
+    char *word;
+}WORD;
+
+int main()
+{
+    int over = 1;//如果程序出现错误就结束
+
+    return 0;
+}
